78512123789c4fe1710dc896b5ee4faadc91f19a
domo1919 <dominik20rr@gmail.com>
2025-06-19 12:01:06 +0200

    add comment, add cmake, add other

--- Zgadywanka/main.c
+++ Zgadywanka/main.c
@@ -4,27 +4,30 @@
 
 int main() {
 
-    srand(time(NULL));
-    int randValue = rand() % 20 + 1;
-    printf("%d\n", randValue);
+    srand(time(NULL));      // Inicjalizacja generatora liczb losowych na podstawie czasu
+    int randValue = rand() % 20 + 1;        // Wylosowanie liczby z zakresu 1–20
+    printf("%d\n", randValue);      // Tymczasowy podgląd wylosowanej liczby (do debugowania)
 
 //    int randValue = rand()%20+1;
 
-    printf("Zgadnij cyfre od 1 do 20.\nPodaj cyfre:\n");
-    int userValue = 0;
-    scanf("%d", &userValue);
+    printf("Zgadnij cyfre od 1 do 20.\nPodaj cyfre:\n");           // Wiadomość zachęcająca do zgadywania
+    int userValue = 0;      // Zmienna na odpowiedź użytkownika
+    scanf("%d", &userValue);        // Odczyt liczby od użytkownika
+
+// Linia wypisująca zgadywaną liczbę użytkownika
 //    printf("\n%d\n", userValue);
 
+ // Pętla dopóki użytkownik nie zgadnie liczby
     while(userValue != randValue){
         if(userValue > randValue){
             printf("Nie zgadles. Liczba jest zbyt duza.\nSprobuj jeszcze raz:\n");
         } else{
             printf("Nie zgadles. Liczba jest zbyt mala.\nSprobuj jeszcze raz:\n");
         }
-        scanf("%d", &userValue);
+        scanf("%d", &userValue);        // Kolejna próba
     }
 
-    printf("Zgadles!");
+    printf("Zgadles!");     // Komunikat po odgadnięciu liczby
 
     return 0;
 }
